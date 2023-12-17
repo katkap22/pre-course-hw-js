4f33555d6365e4ab06b829079a15ca71965fb836
EkaterinaKop <kat_kap@mail.ru>
2023-12-17 15:51:35 +0300

    lesson 2, task 5 completed

---
+++ lesson-02/05.js
@@ -0,0 +1,10 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let copyPassport = {...passport}
+copyPassport.name = "Ivan"
+
+console.log(passport);
+console.log(copyPassport);
