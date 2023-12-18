62bf0337c1a04e32eb62c40e8d7427ee25dd06fc
EkaterinaKop <kat_kap@mail.ru>
2023-12-18 19:16:31 +0300

    lesson 2, task 5 completed

--- lesson-02/04.js
+++ lesson-02/04.js
@@ -1,9 +1,9 @@
 
-const giveMeRightNumbers = (value1, value2, maximum) => {
+const giveMeRightCondition = (value1, value2, maximum) => {
     if (value1 === value2 && value1 <= maximum) {
         console.log('Success!');
     } else {
         console.log('Something wrong!');
     }
 }
-giveMeRightNumbers(101, 100, 200)
+giveMeRightCondition(101, 100, 200)
